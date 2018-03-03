30fd7b2a0f08304274d1a14497a187c799d258b1
aalexeyenko <anna.derevyanko@gmail.com>
2018-03-03 15:52:29 +0100

    Adding first test

---
+++ src/main/java/RunTest.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Ganna Alieksieienko on 03/03/2018.
+ */
+public class RunTest {
+}
---
+++ src/test/java/annotations/SearchForEnergy.java
@@ -0,0 +1,7 @@
+package annotations;
+
+/**
+ * Created by Ganna Alieksieienko on 03/03/2018.
+ */
+public class SearchForEnergy {
+}
